b64920e4f49a0324c215844b2edcc8bb3c733687
Daniel Bruno <daniel.bruno.dev19@gmail.com>
2025-04-16 19:53:26 +0000

    Criação do jogo Pedra-Papel-Tesoura.

---
+++ jogo.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  int escolhaJogador, escolhaPc;
+  srand(time(0));
+
+  printf("Jogo Pedra-Papel-Tesoura!\n");
+  printf("Escolha uma opção abaixo:\n");
+  printf("1. Pedra.\n");
+  printf("2. Papel.\n");
+  printf("3. Tesoura.\n");
+  printf("Sua escolha: ");
+  scanf("%d", &escolhaJogador);
+
+  escolhaPc = rand() % 3 + 1;
+
+
+  switch(escolhaJogador){
+    case 1:
+    printf("Jogador: pedra x ");
+    break;
+    case 2:
+    printf("Jogador: papel x ");
+    break;
+    case 3:
+    printf("Jogador: tesoura x ");
+    break;
+    default:
+    printf("Opção inválida, escolha uma das acima!");
+    break;
+  }
+
+  switch(escolhaPc){
+    case 1:
+    printf ("PC: pedra");
+    break;
+    case 2:
+    printf("PC: papel");
+    break;
+    case 3:
+    printf("PC: tesoura");
+    break;
+  }
+
+  if (escolhaJogador == escolhaPc){
+    printf("\nEmpatou! Tente novamente!\n");
+  } else if ((escolhaJogador == 1) && (escolhaPc == 3) || (escolhaJogador == 2) && (escolhaPc == 1) || (escolhaJogador == 3) && (escolhaPc == 2)){
+              printf("\nParabéns! Você ganhou!\n");
+  } else{
+    printf("\nSinto muito, você perdeu!\n");
+  }
+
+return 0;
+}
